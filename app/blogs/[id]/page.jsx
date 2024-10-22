13f6eb32707cb5792ef41b6edd65a1550308bd0e
Abhay Gupta <abhaygupta@Abhays-MacBook-Air.local>
2024-10-23 01:38:45 +0530

    frontend completed

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { assets, blog_data } from "@/Assets/assets";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+
+const Page = ({ params }) => {
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = () => {
+    for (let i = 0; i < blog_data.length; i++) {
+      if (Number(params.id) === blog_data[i].id) {
+        setData(blog_data[i]);
+        console.log(blog_data[i]);
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+
+  return data ? (
+    <>
+<div className="bg-gray-200 py-5 px-4 md:px-12 lg:px-28">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+          <Image
+            src={assets.logo}
+            width={180}
+            alt=""
+            className="w-[130px] sm:w-auto"
+          />
+          </Link>
+          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-5px_5px_0px_#000000] hover:bg-black hover:text-white transition-all duration-300">
+            Get Started <Image src={assets.arrow} alt="" />{" "}
+          </button>
+        </div>
+        
+        {/* Centering the blog post title and author */}
+        <div className="text-center my-24 max-w-[700px] mx-auto">
+          <h1 className="text-2xl sm:text-5xl font-semibold mx-auto">
+            {data.title}
+          </h1>
+          <Image
+            className="mx-auto mt-6 border border-white rounded-full"
+            src={data.author_img}
+            width={60}
+            height={60}
+            alt=""
+          />
+          <p className="mt-1 pb-2 text-lg">
+            {data.author}
+          </p>
+        </div>
+      </div>
+
+      <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
+        <Image
+          className="border-4 border-white mx-auto"
+          src={data.image}
+          width={1200}
+          height={60}
+          alt=""
+        />
+        <h1 className="my-8 text-[26px] font-semibold ">Introduction:</h1>
+        <p className="text-left">{data.description}</p>
+
+        <h3 className="my-5 text-[26px] font-semibold ">
+          Step: 1 Self-Reflection and Goal
+        </h3>
+        <p className="my-3 text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
+          optio? Consectetur quam dolorem ducimus nemo voluptatum eos vero? Enim
+          inventore ea dignissimos, veritatis error nihil molestias aspernatur
+          vero officiis quis.
+        </p>
+
+        <h3 className="my-5 text-[26px] font-semibold ">
+          Step: 2 Setting Your Plan
+        </h3>
+        <p className="my-3 text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
+          optio? Consectetur quam dolorem ducimus nemo voluptatum eos vero? Enim
+          inventore ea dignissimos, veritatis error nihil molestias aspernatur
+          vero officiis quis.
+        </p>
+
+        <h3 className="my-5 text-[26px] font-semibold ">Step: 3 Execution</h3>
+        <p className="my-3 text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
+          optio? Consectetur quam dolorem ducimus nemo voluptatum eos vero? Enim
+          inventore ea dignissimos, veritatis error nihil molestias aspernatur
+          vero officiis quis.
+        </p>
+        <h3 className="my-5 text-[18px] font-semibold">Conclusion</h3>
+        <p className="my-3">
+          Managing your life style is a journery Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. Cumque beatae non a eum, dolore
+          molestias magni explicabo incidunt error facilis expedita laboriosam
+          tenetur quos consectetur omnis distinctio nisi debitis obcaecati!
+        </p>
+        <div className="my-24">
+          <p className='text-black font-semibold my-4'>Share the artical is social media</p>
+         <div className="flex">
+         <Image src={assets.facebook_icon} width={50} alt=""/>
+         <Image src={assets. twitter_icon} width={50} alt=""/>
+         <Image src={assets. googleplus_icon} width={50} alt=""/>
+         </div>
+        </div>
+      </div>
+      <Footer/>
+    </>
+  ) : (
+    <></>
+  );
+};
+
+export default Page;
